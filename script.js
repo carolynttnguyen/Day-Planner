60f3acda908885fd0b24b796b081717ada57295d
Carolyn Nguyen <carolyntnguyen97@gmail.com>
2020-04-06 04:42:22 -0700

    extra css styling of my own

--- script.js
+++ script.js
@@ -1,77 +1,82 @@
-/////
-// Variavbles
-/////
-var saveBtn = $("saveBtn");
-var currentDate = moment().val
 var text;
 var hour;
 
-/////
-// functions
-/////
- $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
- console.log(moment().format("dddd, MMMM Do YYYY"));
-
-
 $(document).ready(function () {
+    $("#current-day").text(moment().format("dddd, MMMM Do YYYY"));
+    console.log(moment().format("dddd, MMMM Do YYYY"));
     colorChange();
 });
 
 
 
-
 function colorChange() {
-    var realTime = moment().hours();
-    console.log("Current time: " + realTime);
+    var realhour = moment().hours();
+    console.log("Current hour: " + realhour);
 
     $(".event-input").each(function () {
-        var timeTest = parseInt($(this).attr("id"));
-        console.log(timeTest);
+        var hourTest = parseInt($(this).attr("id"));
+        console.log(hourTest);
 
-        if (realTime > timeTest) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
-        } else if (realTime < timeTest) {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
-        } else {
+        if (realhour === hourTest) {
             $(this).removeClass("future");
             $(this).removeClass("past");
             $(this).addClass("present");
+        } else if (realhour > hourTest) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");           
+        } else {
+            
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
         }
     });
 }
 
-
+function newScheduleData() {
+    return [
+        { hour: 9, text: "" },
+        { hour: 10, text: "" },
+        { hour: 11, text: "" },
+        { hour: 12, text: "" },
+        { hour: 13, text: "" },
+        { hour: 14, text: "" },
+        { hour: 15, text: "" },
+        { hour: 16, text: "" },
+        { hour: 17, text: "" },
+    ]
+};
 
 //function to combine new and previous data and update the block
-function updateTimeBlocks() {
+function updatehourBlocks() {
     var scheduleData = getScheduleData();
 
 };
 //function getting any previous data or input 
-function getScheduleData() {;
-    var scheduleData = localStorage.getItem(currentDate);
-    //if there is not previous data, then create new data, calling  createNewData function 
-    if (!scheduleData) {
-        scheduleData = newScheduleData();
-    } return scheduleData;
+function getScheduleData() {
+    //load any saved data from LocalStorage - do this for each hour created.
+    $("#9 .event-input").val(localStorage.getItem("9"));
+    $("#10 .event-input").val(localStorage.getItem("10"));
+    $("#11 .event-input").val(localStorage.getItem("11"));
+    $("#12 .event-input").val(localStorage.getItem("12"));
+    $("#13 .event-input").val(localStorage.getItem("13"));
+    $("#14 .event-input").val(localStorage.getItem("14"));
+    $("#15 .event-input").val(localStorage.getItem("h15"));
+    $("#16 .event-input").val(localStorage.getItem("16"));
+    $("#17 .event-input").val(localStorage.getItem("17"));
 };
 
-function newScheduleData() {
 
-};
 
-saveBtn.click(function() {
+$("saveBtn").click(function () {
     text = $(this).siblings(".event-input").val();
     console.log(text);
-    hour = $(this).siblings(".hour").text();
+   hour = $(this).siblings(".hour").text();
     console.log(hour);
     localStorage.setItem(hour, JSON.stringify(text));
-    
-    colorChange ();
-    newScheduleData ();
-})
+
+    colorChange();
+    newScheduleData();
+});
 
