09f45029cf748a273c96606d6d2df03c3f3882cf
Carolyn Nguyen <carolyntnguyen97@gmail.com>
2020-04-06 02:47:23 -0700

    minor changes to JS code style

--- script.js
+++ script.js
@@ -1,107 +1,77 @@
-var date = new Date();
-var currentMonth = date.getMonth() + 1;
-var currentDay = date.getDate();
-var currentHour = date.getHours();
-var currentMinute = date.getMinutes();
-var currentSecond = date.getSeconds()
-
-//currentMonth has values 1-12, currentDay has values 1-31, currentHour... etc.
-console.log(currentMonth, currentDay, currentHour, currentMinute, currentSecond)
-
-
-var currentTime = moment().format("HH");
-var currentTimeInt = parseInt(currentTime);
+/////
+// Variavbles
+/////
 var saveBtn = $("saveBtn");
+var currentDate = moment().val
+var text;
+var hour;
 
+/////
+// functions
+/////
+ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+ console.log(moment().format("dddd, MMMM Do YYYY"));
 
-//set data att to each hour element
-$("#nine-row").attr("data-time", moment("9:00am" , "h:mm a").format("HH"));
-$("#ten-row").attr("data-time", moment("10:00am" , "h:mm a").format("HH"));
-$("#eleven-row").attr("data-time", moment("11:00am" , "h:mm a").format("HH"));
-$("#twelve-row").attr("data-time", moment("12:00am" , "h:mm a").format("HH"));
-$("#one-row").attr("data-time", moment("1:00am" , "h:mm a").format("HH"));
-$("#two-row").attr("data-time", moment("2:00am" , "h:mm a").format("HH"));
-$("#three-row").attr("data-time", moment("3:00am" , "h:mm a").format("HH"));
-$("#four-row").attr("data-time", moment("4:00am" , "h:mm a").format("HH"));
-$("#five-row").attr("data-time", moment("5:00am" , "h:mm a").format("HH"));
 
 $(document).ready(function () {
-    setup()
+    colorChange();
 });
 
-//ADD CURRENT DATE TO TOP OF PAGE
-function setup() {
-    displayCurrentDate();
-    //    updateTimeBlocks();
 
-};
 
-//function to display current date
-function displayCurrentDate() {
-    var currentDate = getCurrentDate();
-    $("#display-current-date").text(currentDate);
-};
 
-//function to get current date
-function getCurrentDate() {
-    var date = moment().format("dddd, MMMM Do");
-    return date;
-};
+function colorChange() {
+    var realTime = moment().hours();
+    console.log("Current time: " + realTime);
+
+    $(".event-input").each(function () {
+        var timeTest = parseInt($(this).attr("id"));
+        console.log(timeTest);
+
+        if (realTime > timeTest) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        } else if (realTime < timeTest) {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        } else {
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+    });
+}
+
 
 
 //function to combine new and previous data and update the block
 function updateTimeBlocks() {
-    var scheduleData = getScheduleData()
+    var scheduleData = getScheduleData();
 
 };
-
 //function getting any previous data or input 
-function getScheduleData() {
-    var currentDate = getCurrentDate();
+function getScheduleData() {;
     var scheduleData = localStorage.getItem(currentDate);
+    //if there is not previous data, then create new data, calling  createNewData function 
     if (!scheduleData) {
-        scheduleData = createNewSchData();
+        scheduleData = newScheduleData();
     } return scheduleData;
 };
 
+function newScheduleData() {
 
-//CHANGING INPUT BOX COLOR BASED ON TIME OF DAY
-
-function boxColor() {
-    //set a variable input time to represent the input block's corresponding time with a for loopp
-    for ( var i = 1; i <=12; i++) {
-        var inputTime = $("#" + i + "Row").attr("data-time");
-        var inputTimeInt = parseInt(inputTime);
-        console.log (inputTimeInt);
-
-        if (inputTimeInt === currentTimeInt ) {
-            $("#" + i + "Row").addClass("present");
-        }
-        else if (inputTimeInt > currentTimeInt) {
-            $("#" + i + "Row").addClass("past");
-        }
-        else (inputTimeInt < currentTimeInt) {
-            $("#" + i + "Row").addClass("future");
-        }
-    }
 };
 
+saveBtn.click(function() {
+    text = $(this).siblings(".event-input").val();
+    console.log(text);
+    hour = $(this).siblings(".hour").text();
+    console.log(hour);
+    localStorage.setItem(hour, JSON.stringify(text));
+    
+    colorChange ();
+    newScheduleData ();
+})
 
-//BUTTON HOVER
-saveBtn.on("mouseenter", function () {
-    $(this).addClass('hover');
-});
-
-saveBtn.on("mouseleaves", function () {
-    $(this).renoveClass('hover');
-});
-
-//function for save button
-saveBtn.on("click", function () {
-    var hour = $(this).attr("data-hour");
-
-    var plan = $("#" + hour + "Row").val();
-
-    localStorage.setItem(time, text);
-
-});
